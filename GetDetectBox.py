5a9a8dadcdaffd46770612df17fc113565088719
thousandyoung <muji_cullin@qq.com>
2021-10-31 14:30:45 +0800

    add getDetectBox using paddleOcr

---
+++ GetDetectBox.py
@@ -0,0 +1,8 @@
+import os
+def GenerateBox(imagePath):
+    flag = os.system("python3 PaddleOCR-release-2.3/tools/infer/predict_det.py --det_algorithm=\"SAST\" --image_dir=\"{}\" --det_model_dir=\"PaddleOCR-release-2.3/inference/det_sast_tt/\" --det_sast_polygon=True --use_gpu=False".format(imagePath))
+    if flag == 1:
+        print("generate box failed")
+
+GenerateBox("./images/panel1.png")
+
